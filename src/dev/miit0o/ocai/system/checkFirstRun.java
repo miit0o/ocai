f0f1fa49bb6ccf697ab4bba4b8f8952f2c910c4c
Christoph <christoph@rustige.me>
2022-03-30 11:41:16 +0200

    minor changes i didn't complete because I was tired

---
+++ src/dev/miit0o/ocai/system/checkFirstRun.java
@@ -0,0 +1,11 @@
+package dev.miit0o.ocai.system;
+
+public class checkFirstRun {
+
+    public static boolean isFirstRun(){
+        //Check if OCAI is running for the first time
+
+        return false;
+    }
+
+}
---
+++ src/dev/miit0o/ocai/system/setup.java
@@ -0,0 +1,7 @@
+package dev.miit0o.ocai.system;
+
+public class setup {
+    //This class is used to create the setup process of OCAI.
+    //Important and required packages will be installed here.
+
+}
